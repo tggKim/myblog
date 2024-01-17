ef1ed12c7a6be51c556423301fa6a9f29f90b1f6
tggKim <138564756+tggKim@users.noreply.github.com>
2024-01-17 23:42:18 +0900

    Delete myApi01 directory

--- myApi01/src/main/java/kim/myApi01/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package kim.myApi01.repository;
-
-import kim.myApi01.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item,Long> {
-}
